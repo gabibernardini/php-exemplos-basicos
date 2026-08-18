5c3c489f809950bfd97f4c79558eb938f541562e
gabibernardini <gabriellabernardinigc@gmail.com>
2026-08-18 16:15:51 -0300

    Assunto 2
    Inclusão de material.

---
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro - Status Codes</title>
+</head>
+<body>
+
+    <h1>Cadastro de Aluno (com Status Code)</h1>
+    <form action="" method="post"></form>
+        <label for="nome">Nome:</label>
+        <input type="text" name ="nome" required><br><br>
+
+        <label for="idade">Idade:</label>
+        <input type="text" name ="idade" required><br><br>
+
+        <button type="submit">Enviar</button>
+
+</form>
+
+
